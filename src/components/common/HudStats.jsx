7b687c96d338a677da02bc409c3135651b6d2554
Rudra Rathod <rudrarathod738@gmail.com>
2026-06-09 17:27:10 +0530

    refactor: remove wheat resource display from HudStats component

--- src/components/common/HudStats.jsx
+++ src/components/common/HudStats.jsx
@@ -8,7 +8,6 @@ import { useGameStore } from '../../store/gameStore.js'
  */
 export default function HudStats() {
   const xp = useGameStore((state) => state.xp)
-  const wheat = useGameStore((state) => state.wheat)
   const energy = useGameStore((state) => state.energy)
   const maxEnergy = useGameStore((state) => state.maxEnergy)
 
@@ -19,11 +18,6 @@ export default function HudStats() {
       <div className="hud-stat" title="Harvest Experience">
         <MaterialIcon icon="star" filled className="hud-stat-icon hud-stat-icon--amber" size="18px" />
         <span className="hud-stat-value font-label-tech">XP: {xp.toLocaleString()}</span>
-      </div>
-      <div className="hud-divider" />
-      <div className="hud-stat" title="Wheat Harvested">
-        <MaterialIcon icon="agriculture" filled className="hud-stat-icon hud-stat-icon--blue" size="18px" />
-        <span className="hud-stat-value font-label-tech">WHEAT: {wheat}</span>
       </div>
       <div className="hud-divider" />
       <div className="hud-stat" title="Drone Energy">
